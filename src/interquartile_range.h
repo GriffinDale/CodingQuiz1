acb6906f09afa9b182359bf07345d847788381c8
GriffinDale <116691203+GriffinDale@users.noreply.github.com>
2024-01-27 16:30:12 -0500

    Update interquartile_range.h

--- src/interquartile_range.h
+++ src/interquartile_range.h
@@ -1,27 +1,13 @@
-/*
-    Interquartile Range
-
-    Given a sorted singly linked list without a tail (e.g, head -> 1 -> 2 -> 3 -> 4), 
-    return the interquartile range of the data set using the slow and fast pointer 
-    approach OR using a methodology that does not iterate over the linked list 
-    twice. You must not iterate over the entire linked list more than once and you 
-    cannot use arrays, vectors, lists or an STL implementation of List ADT in this problem.
-
-    Sample Input:
-        2 4 4 5 6 7 8
-    
-    Sample Output:
-        3.00 
-*/
-
 #include <iostream>
 #include <iomanip>
+
 
 class Node {
     public:
         int value;
         Node* next = nullptr;
 };
+
 
 Node* insertEnd(Node* head, int key)
 {
@@ -39,8 +25,27 @@ Node* insertEnd(Node* head, int key)
     return head;
 }
 
+
 float interQuartile(Node* head)
 {
-    //code here
-    return 0.0;   
+    Node* fast = head->next;
+    Node* q1Node = head;
+    Node* q3Node = head;
+    int count = 2;
+    float q1, q3 = 0;
+    while(fast->next) {
+        fast = fast->next;
+        if ((count-1) % 4 == 0) q1Node = q1Node->next;
+        if ((count+3) % 4 != 0) q3Node = q3Node->next;
+        count++;
+    }
+    if ((int((count) / 2) + 1) % 2 == 0) {
+        q1 = q1Node->value;
+        q3 = q3Node->next->value;
+    }
+    else {
+        q1 = float(q1Node->value + q1Node->next->value) / 2.0;
+        q3 = float(q3Node->value + q3Node->next->value) / 2.0;
+    }
+    return q3-q1;  
 }
